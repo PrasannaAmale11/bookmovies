17f35811921880526b8390a245a24524f6701faf
Prasanna Amale <110029115+PrasannaAmale11@users.noreply.github.com>
2023-05-04 16:54:08 +0530

    updated Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route,Routes} from "react-router-dom";
+// import {Switch} from 'react-router-dom/dist/react-router-dom.development'
 import './App.css';
+import MovieList from './components/MovieList';
+import MovieSummary from './components/MovieSummary';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path='/' Component={MovieList}/>
+      
+      
+      
+      <Route exact path='/show/:movieId' Component={MovieSummary}/>
+        
+      </Routes> 
+      </Router>
     </div>
   );
 }
